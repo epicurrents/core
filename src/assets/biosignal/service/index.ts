f0606c4594e2eaef6602b3efc7e419ae2db71501
sam-19 <26563023+sam-19@users.noreply.github.com>
2023-12-03 16:30:39 +0200

    Export SharedWorkerCache
    Signed-off-by: sam-19 <26563023+sam-19@users.noreply.github.com>

--- src/assets/biosignal/service/index.ts
+++ src/assets/biosignal/service/index.ts
@@ -1,9 +1,11 @@
 import BiosignalMutex from "./BiosignalMutex"
 import GenericBiosignalService from "./GenericBiosignalService"
 import MontageService from "./MontageService"
+import SharedWorkerCache from "./SharedWorkerCache"
 
 export {
     BiosignalMutex,
     GenericBiosignalService,
     MontageService,
+    SharedWorkerCache,
 }
