24ea8f01b17c0a3d0faed82d0eac5561c46c4656
sam-19 <26563023+sam-19@users.noreply.github.com>
2025-02-18 20:06:46 +0200

    Don't reset name
    Signed-off-by: sam-19 <26563023+sam-19@users.noreply.github.com>

--- src/assets/biosignal/components/GenericBiosignalSetup.ts
+++ src/assets/biosignal/components/GenericBiosignalSetup.ts
@@ -77,8 +77,6 @@ export default class GenericBiosignalSetup implements BiosignalSetup {
                 unit: config?.unit || '?',
             } as SetupChannel
         }
-        // Use config name if present.
-        this._name = config.label || this._name
         // Don't rematch already matched signals.
         const matchedSigs = [] as number[]
         // Try to match config channels to record signals.
